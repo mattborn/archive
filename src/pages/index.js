3861970949f4ca3366ec0ddbffa61c5e79145f47
Matt Born <mattborn@users.noreply.github.com>
2018-10-03 14:22:38 -0600

    Swing batta batta

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,118 @@ import React from "react"
 
 export default () => (
   <div>
-    <h1>25 Sep 2018 · Deploying something new, so hold tight…</h1>
+    <div style={{margin: 'auto', maxWidth: 800}}>
+      <h1>Selected Work —</h1>
+      <p>This is a work in progress. Thanks for visiting!</p>
+      <h2>Ridgeline</h2>
+      <img alt="Ridgeline" src="https://i.postimg.cc/YkBsGxxN/Ridgeline.png"/>
+      <div class="flex">
+        <div>
+          <h3>Zero to one</h3>
+          <p>In summer 2017, the pastor who married us asked if we would move to Utah to start a Christian church. We said no. Then we understood how important this was, moved away from family, and went all-in to build something incredible from nothing.</p>
+        </div>
+        <div>
+          <h3>Analog and digital</h3>
+          <p>We needed everything. A new logo and type system. Pitch decks. Several iterations of a website. Micro-sites and forms. Video bumpers. Marketing graphics. Announcement slides. Big signs. Small signs. A neon sign. Ridiculous 30-foot signs.</p>
+        </div>
+        <div>
+          <h3>Design systems for churches</h3>
+          <p>The best thing about my work for Ridgeline is that tons of it is being used by other churches all over the country through my partnership with MyXP. Since day one, we have been committed to helping churches thrive. As many as we can.</p>
+        </div>
+        <div>
+          <h3>High risk, high reward</h3>
+          <p>Hands down, this is the biggest leap of faith my wife and I have ever taken. On Sunday, September 30, 2018 we held our launch service with 145 folks in attendence including 25 new families. That’s amazing for anywhere, but especially Salt Lake.</p>
+        </div>
+      </div>
+      <div><a href="http://ridgelineslc.com/" target="_blank">Check out the Ridgeline website &rarr;</a></div>
+      <h2>Bambu</h2>
+      <img alt="Bambu" src="https://i.postimg.cc/4JKStLMP/Bambu.png"/>
+      <div class="flex">
+        <div>
+          <h3>Employee advocacy</h3>
+          <p>Four years ago, I didn’t know what this was. Most people still don’t. Yet we were early to market and I led code, pixels, and people as we scaled this to a multi-million dollar enterprise SaaS product.</p>
+        </div>
+        <div>
+          <h3>Zero to two</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Highlight 2</h3>
+          <p>Normalize a consumption experience including dozens of media and source types in an intuitive way.</p>
+        </div>
+        <div>
+          <h3>Let’s NOT build this</h3>
+          <p>With such a small team attracting the largest companies in the world, we needed to say NO at the right times. Zapier instead of RSS.</p>
+        </div>
+        <div>
+          <h3>People management</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Product management</h3>
+          <p>…</p>
+        </div>
+      </div>
+      <h2>Research</h2>
+      <img alt="Research" src="https://i.postimg.cc/Jm3dbBTf/Research.png"/>
+      <div class="flex">
+        <div>
+          <h3>Persona development</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Design thinking</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Staying in the medium</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Human-centered outcomes</h3>
+          <p>…</p>
+        </div>
+      </div>
+      <h2>Systems Design</h2>
+      <img alt="Systems" src="https://i.postimg.cc/HYXvvJT7/Systems.png"/>
+      <div class="flex">
+        <div>
+          <h3>Component library</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Design as discipline</h3>
+          <p>Defining the standards for display made it quick to champion consistency by including references to past decisions in every project moving forward.</p>
+        </div>
+        <div>
+          <h3>Universal components</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Passing the baton</h3>
+          <p>…</p>
+        </div>
+      </div>
+      <h2>Sprout Social</h2>
+      <img alt="Sprout" src="https://i.postimg.cc/WpVWB24Y/Sprout.png"/>
+      <div class="flex">
+        <div>
+          <h3>Always be growing</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Radical design</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>A little bit of everything</h3>
+          <p>…</p>
+        </div>
+        <div>
+          <h3>Planting seeds</h3>
+          <p>Before switching products, I prototyped way more experiences than we could test with customers. It was amazing to see my ideas grow to reach production without my direct involvement.</p>
+        </div>
+      </div>
+    </div>
   </div>
 )
